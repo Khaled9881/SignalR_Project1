f5c7a6420f757144c8868a36140b42cd625837dd
khaled Ibrahiem <khaled.ibrahiem@msa.edu.eg>
2026-06-14 18:17:03 +0300

    Add project files.

---
+++ SignalR_Project1/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Project1.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalR_Project1/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Project1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void sendMessage(string name, string message)
+        {
+            // receive messag and store it in DB
+
+
+            // Broadcating message to clients
+
+
+            Clients.All.SendAsync("newMessage", name, message);
+        }
+
+    }
+}
